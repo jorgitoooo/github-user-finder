5d96ef70789dd580308a5d074153f479dc52afb8
Jorge Garcia <jlg.sftdev@gmail.com>
2019-06-30 02:48:39 -0700

    Class based components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <NavBar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
