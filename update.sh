da54839091562033f6eae34810ca06e3426169e9
Jorge Garcia <jlg.sftdev@gmail.com>
2019-06-30 18:55:05 -0700

    Refactored and added a spinner

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r ../github-finder/package-lock.json \
+      ../github-finder/package.json \
+      ../github-finder/public \
+      ../github-finder/src \
+      ../github-finder/.gitignore \
+      ../github-finder/.env.local ./
